1c07b9b44c30913d75323203ca810a9d535941ca
Korada Nagesh Kumar <nageshkorada66@gmail.com>
2026-03-11 20:30:08 +0530

    First Commit in First Branch.

--- app/src/main/java/com/example/androidtesting/MainActivity.kt
+++ app/src/main/java/com/example/androidtesting/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidtesting.ui.theme.AndroidTestingTheme
 
@@ -35,6 +36,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+    Text(
+        text = "Hello Nagesh",
+        color = Color.Green
     )
 }
 
